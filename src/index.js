333b0f890fc694e20330986f63c0b8eedba6222a
Keisuke Kan <9renpoto@gmail.com>
2016-12-31 16:52:43 +0900

    feat(test) add tests

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 /* @flow */
 import 'bulma.sass'
 import 'highlight.css'
-import { initHighlightingOnLoad } from 'highlight.js'
-initHighlightingOnLoad()
 
-import h337 from 'heatmap.js'
-(body => {
-  const heatmap = h337.create({
-    container: body,
-    maxOpacity: 0.5
-  })
-
-  body.addEventListener('mousemove', (e: { pageX: number, pageY: number }) => {
-    const { pageX, pageY } = e
-    const point = {x: pageX, y: pageY}
-    const prev: number = heatmap.getValueAt(point) || 0
-    heatmap.addData(Object.assign({}, point, {value: prev + 1}))
-  })
-})(document.body)
+import './heatmap'
+import './highlight'
---
+++ test/highlight.test.js
@@ -0,0 +1,9 @@
+/* @flow */
+import { describe, it } from 'mocha'
+import assert from 'assert'
+
+describe('highlight', () => {
+  it('load', () => {
+    assert(require('../src/highlight'))
+  })
+})
